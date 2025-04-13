e38f635d5987a0bebbe50215fbca4768e414e0d5
0xTomoyo <93022570+0xTomoyo@users.noreply.github.com>
2025-04-12 18:23:39 -0700

    Update NFTSVG.sol

--- contracts/libraries/NFTSVG.sol
+++ contracts/libraries/NFTSVG.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.7.6;
 
 import '@openzeppelin/contracts/utils/Strings.sol';
 import '@uniswap/v3-core/contracts/libraries/BitMath.sol';
-import 'base64-sol/base64.sol';
+import './base64.sol';
 
 /// @title NFTSVG
 /// @notice Provides a function for generating an SVG associated with a Uniswap NFT
